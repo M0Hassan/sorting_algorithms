f8e869047d30b8106bb87a8a14b6d35755d80c0a
Mohamed Hassan <aminmohamedadan@gmail.com>
2021-10-25 09:07:40 -0700

    function that sorts an array of integers using the Quick Sort algorithm

--- sort.h
+++ sort.h
@@ -35,6 +35,8 @@ void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
+void quick_sort_helper(int *arr, int start, int end, size_t size);
+int lomuto_partition(int *arr, int start, int end, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
